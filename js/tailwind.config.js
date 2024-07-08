cb5af68204c0b388d7b04a992570309e4dfb8dcc
lehonprime <lehonprime@gmail.com>
2024-07-08 18:26:03 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        
+        colors: {
+          clifford: '#da373d',
+        },
+        fontFamily: {
+           sans : ['Open Sans'],
+           pap : ['Paprika'],
+           
+          },
+          maxWidth: {
+            container : '1240px',
+          },
+          backgroundImage: {
+            bnimgg : "url('./images/bnimg.png')",
+            bnimggg : "url('./images/Image.png')",
+           
+          },
+          
+      }
+    }
+  }
